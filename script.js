904dc41e2f3615e8aae335532b90dab5dde10d74
github_username <nedimmuminovic61@gmail.com>
2023-05-03 13:59:01 +0200

    finnishing an assigment the course instructor gave me

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let age = 20;
 
 console.log("my name is: ", name, "i am", age, "years old")
 
+let country = "Serbia";
+
+let continent = "Europe";
+
+let population = "8.6 milion"
+
+console.log(country, continent, population)
+
