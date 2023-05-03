3451dee6522ad239499616cb217c84929b4a8347
github_username <nedimmuminovic61@gmail.com>
2023-05-03 14:38:12 +0200

    finnishing the assigment of the 'Data type's lecture

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ let population = "8.6 milion"
 console.log(country, continent, population)
 */
 
+let isIsland = false;
+
+let language;
+
+let population = 8639112;
+
+let country = "Serbia";
+
+console.log(typeof isIsland)
+console.log(typeof language)
+console.log(typeof population)
+console.log(typeof country)
+
