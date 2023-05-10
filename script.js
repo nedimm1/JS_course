213aeec69b41e678a3e0bf5ede1a10c45bd62bad
github_username <nedimmuminovic61@gmail.com>
2023-05-10 17:42:13 +0200

    adding another if statement

--- script.js
+++ script.js
@@ -96,9 +96,18 @@ console.log("a\n\sentance\n\with\n\spaces")
 */
 
 const age = 15;
-let yearsLeft = 18;
+
 if(age >= 18){
     console.log("U can drive")
 }else{
+    const yearsLeft = 18;
     console.log(`u are too young, you have to wait ${yearsLeft - age} more years`)
 }
+
+const birthyear = 2008;
+if(birthyear <= 2000){
+    let century = 20;
+}else{
+    century = 21;
+}
+console.log(century);
