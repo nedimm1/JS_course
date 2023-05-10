a629e188260aa728b86ea42da2684298094ff0e6
github_username <nedimmuminovic61@gmail.com>
2023-05-10 16:28:58 +0200

    a sentance with spaces

--- script.js
+++ script.js
@@ -91,3 +91,5 @@ const birthyear = 2008;
 console.log("hi i am " + name, ", i am a " + occupation, "at a " + schoolName, ", and i am " + (currentYear - birthyear), "years old");
 
 console.log(`hi i am ${name}, i am a ${occupation} at a ${schoolName}, and i am ${currentYear - birthyear} years old`)
+
+console.log("a\n\sentance\n\with\n\spaces")
