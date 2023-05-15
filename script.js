0481cfbf3963199dc946a68e0d46a01112a85859
github_username <nedimmuminovic61@gmail.com>
2023-05-15 15:15:46 +0200

    truthy and falsy values

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ console.log(Number(num) + 15);
 
 console.log(String(25), 25);
 
-*/
+
 
 console.log("20" + "20" - 12);
 
@@ -152,3 +152,10 @@ let n = '1' + '1';
 n = n - 1;
 
 console.log(n);
+*/ 
+
+console.log(Boolean(0));
+console.log(Boolean(undefined))
+console.log(Boolean('me'))
+console.log(Boolean({}))
+console.log(Boolean(''))
