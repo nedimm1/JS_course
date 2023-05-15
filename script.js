724ede6c3be902f950c23c26d1d018f9172c2e69
github_username <nedimmuminovic61@gmail.com>
2023-05-15 15:18:48 +0200

    an example for truthy and falsy values

--- script.js
+++ script.js
@@ -152,10 +152,17 @@ let n = '1' + '1';
 n = n - 1;
 
 console.log(n);
-*/ 
+*/
 
 console.log(Boolean(0));
 console.log(Boolean(undefined))
 console.log(Boolean('me'))
 console.log(Boolean({}))
 console.log(Boolean(''))
+
+money = 0;
+if (money) {
+    console.log('dont spend too much')
+} else { 
+    console.log('no money :(') 
+}
