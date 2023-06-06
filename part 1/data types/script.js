5aa00bd39c8acdd930c4898dc0493214b377faac
github_username <nedimmuminovic61@gmail.com>
2023-06-06 21:10:03 +0200

    data types lecture

---
+++ part 1/data types/script.js
@@ -0,0 +1,15 @@
+let isIsland = false; //boolean = true & false
+
+let language; //undefined = declared variable that dose not have a value at the time
+
+let population = 8639112; //int = numbers
+
+let country = "Serbia"; //string = letters and symbols
+
+let coastline = null; //null an empty value
+
+console.log(typeof isIsland)
+console.log(typeof language)
+console.log(typeof population)
+console.log(typeof country)
+console.log(coastline)
