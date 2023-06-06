ab2c910b82864a62f87cf1361dd7b324d10da1d0
github_username <nedimmuminovic61@gmail.com>
2023-06-06 18:42:29 +0200

    adding comments

--- part 1/hello world/script.js
+++ part 1/hello world/script.js
@@ -1,7 +1,14 @@
+
 let name = "Nedim";
+//let is a keyword that can help declear a variable
+//"Nedim" is a value, the smallest piece of info in JS. 
+//name is a variable, wich stores the value and that allowes us to use the value over and over again in diferent places.
+//the ';' is used to tell JS that i am done with this line(not necessary)
 let age = 20;
 
 console.log("my name is: ", name, "i am", age, "years old")
+//console.log() is a function. It allows developers to output information to the browser's debugging console, 
+//which is a tool used for testing and debugging JS code
 
 let country = "Serbia";
 
