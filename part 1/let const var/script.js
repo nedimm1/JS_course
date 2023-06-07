8af02e2fdc727a34628ed40fb3d1630a6028cee5
github_username <nedimmuminovic61@gmail.com>
2023-06-07 17:36:27 +0200

    let const and var

--- part 1/let const var/script.js
+++ part 1/let const var/script.js
@@ -1,9 +1,9 @@
-let languagesAlsoSpoken;
-
+let languagesAlsoSpoken; 
+//can be changed only inside the block where its defined
 languagesAlsoSpoken = "serbian, bosnian and croatian";
 
-const nativeLanguage = "serbian";
+const nativeLanguage = "serbian";//cannot be changed and cant be undefined
 
-var otherLanguages;
+var otherLanguages;//can be changed wherever inside the function
 
 otherLanguages = "serbian", "bosnian", "croatian"
