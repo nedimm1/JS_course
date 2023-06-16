4decfc1177d8c99964fa9c566598301a7c5b5d65
github_username <nedimmuminovic61@gmail.com>
2023-06-16 16:30:23 +0200

    template literals lecture

--- part 1/string literals/script.js
+++ part 1/string literals/script.js
@@ -8,9 +8,21 @@ const currentYear = 2023;
 
 const birthyear = 2008;
 
-
+//instead of combining strings and variables like this:
 console.log("hi i am " + name, ", i am a " + occupation, "at a " + schoolName, ", and i am " + (currentYear - birthyear), "years old");
 
+//a more simpler solution would be to use a template literal:
 console.log(`hi i am ${name}, i am a ${occupation} at a ${schoolName}, and i am ${currentYear - birthyear} years old`)
 
+//with template literals we can write a more normal looking string and insert varaibles in it
+//you can also write a normal string like this
+
+//also, instead of writing a string with multiple lines 
 console.log("a\n\sentance\n\with\n\spaces")
+// /n = new line
+
+//with a template literal you can write it like this:
+console.log(`a
+sentance
+with
+spaces`)
