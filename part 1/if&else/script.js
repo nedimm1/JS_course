ecae0ad9e121d6d76e153edf09d6b80e97fb3cdc
github_username <nedimmuminovic61@gmail.com>
2023-06-18 13:33:19 +0200

    if/else lecture

--- part 1/if&else/script.js
+++ part 1/if&else/script.js
@@ -1,6 +1,7 @@
-if(age >= 18){
+//if/else statement is a control structure used to make decisions and execute different blocks of code based on certain conditions.
+if(age >= 18){//tis is the condition, an expression that evaluates to either true or false. 
     console.log("U can drive")
-}else{
+}else{//an alternative code block in case the previous statement is false
     const yearsLeft = 18;
     console.log(`u are too young, you have to wait ${yearsLeft - age} more years`)
 }
@@ -12,3 +13,14 @@ if(birthyear <= 2000){
     century = 21;
 }
 console.log(century);
+
+//we can also use "else if" statements which is another control structure which provides another condition in case the previous
+//condition was false
+//looks something like this
+if (condition1) {
+    //  block of code to be executed if condition1 is true
+  } else if (condition2) {
+    //  block of code to be executed if the condition1 is false and condition2 is true
+  } else {
+    //  block of code to be executed if the condition1 is false and condition2 is false
+  }
