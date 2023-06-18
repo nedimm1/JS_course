f28e6d9b4202d72d293819e01214642b90d417ba
github_username <nedimmuminovic61@gmail.com>
2023-06-18 17:19:54 +0200

    switch statement lecture

--- part 1/switch statement/script.js
+++ part 1/switch statement/script.js
@@ -1,4 +1,7 @@
-const day = 'thursday';
+//the switch statement is used to make decisions based on the value of a given expression. 
+//It allows you to compare the value of the expression against multiple cases and execute different blocks of code 
+//based on the matching case.
+const day = prompt("what day is it? ")
 
 switch(day) {
     case 'monday':
@@ -21,6 +24,6 @@ switch(day) {
     case 'sunday':
         console.log("its sunday");
         break;
-    default:
+    default: //in case expression dosnt match any of the cases the default case is executed
         console.log("thats not a day");                
 }
