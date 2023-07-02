b3831b80f9d8720a52d1d74acfed5b81f8b255fb
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-02 18:02:08 +0200

    array lecture

---
+++ part 2/Arrays/script2.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//an array is an ordered collection of values. 
+//It allows you to store multiple values in a single variable, forming a list-like structure. 
+
+//instead of stroring multiple values like this:
+
+const familyMember1 = "Dad";
+const familyMember2 = 'Mom';
+const familyMember3 = 'brother'
+
+//more eficient way would be like this:
+
+//first example of an array:
+
+//Each value in an array is called an element, 
+//and elements are separated by commas and enclosed within square brackets [].
+const familyMembers = ['Dad', 'mom', 'brother'];
+
+//second example of an array:
+
+const years = new Array(1999, 2008, 2020)
+
+//and now we can print all of this values at once
+console.log(familyMembers);
+console.log(years);
+
+//we can also print specific values from thees arrays:
+console.log(familyMembers[1], familyMembers[0])
+
+//we can check the lenght of the array:
+console.log(familyMembers.length)
+
+//mutate(change) the value of a specific value in a array:
+familyMembers[2] = 'me'//the reasone why this can be changed, even though its const, is beacuse that
+//rule that says that const cant be changed only  applies to primitive values. 
+//an array is not a primitive value
+console.log(familyMembers)
+
+//we can include other variables and arrays into an array:
+const name = 'Nedim';
+const lastName = 'Muminovic';
+const age = 15;
+
+const nedim = [name, lastName, age, familyMembers]
+
+console.log(nedim)
