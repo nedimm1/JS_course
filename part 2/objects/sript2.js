683539d25a6e52210afac1a0eb7aefd7c31b0fc4
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-05 16:23:37 +0200

    object lecture

---
+++ part 2/objects/sript2.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//n JavaScript, an object is a complex data type 
+//that allows you to store and organize related data and functionality together.
+
+const me = [
+    'nedim',
+    2023 - 2008,
+    'student',
+    ['mom', 'dad', 'brother']
+]
+
+//arrays store homogeneous data and have numeric indices
+
+const nedim = {
+    name: 'nedim',//these are properties 
+    age: 2023 - 2008,//Properties define the data or behavior of an object.
+    occupation: 'student',
+    familyMembers: ['mom', 'dad', 'brother']
+}
+//while objects properties have meaningful names and can store any type of value (PRIMITIVE values)
