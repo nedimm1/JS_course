fdc71bdaf51d46f6f5247467dc22465a450ed950
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-06 17:57:47 +0200

    creating a better function

--- part 2/object methods/script2.js
+++ part 2/object methods/script2.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const nedim = {
+const nedim2 = {
     name: 'nedim',
     birthYear: 2008,
     occupation: 'student',
     familyMembers: ['mom', 'dad', 'brother'],
 
-    calcAge: function(birthYear){
+   /* calcAge: function(birthYear){
         return 2023 - birthYear
+    }*/
+
+    calcAge: function(){
+        return 2023 - this.birthYear // the 'this' keyword provides a way to access and operate 
+        //on the properties and methods of the object that owns or calls the function.
     }
 }
 
-console.log(nedim.calcAge(2008));
-console.log(nedim['calcAge'](2008));
+console.log(nedim2.calcAge());//we can even change the name of the function and it will work the same
+//console.log(nedim['calcAge'](2008));
