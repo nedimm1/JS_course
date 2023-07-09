ba1abd29850ad19042f2653afc6c41d7eec6512b
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-09 14:16:16 +0200

    foor loop lecture

---
+++ part 2/the foor loop/script2.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//loops are a fundemental aspect of every programming language
+//they allow us to automate repetetive tasks
+
+//a for loop allows us  to repeat a block of code for a specified number of iterations.
+
+//logging a message like this dose not follow the D.R.Y(Don't Repeat Yourself) principle:
+/*console.log('message  number 1')
+console.log('message  number 2')
+console.log('message  number 3')
+console.log('message  number 4')
+console.log('message  number 5')*/
+
+//a more easier way is to use a for loop
+for(let messageNumber = 1; messageNumber <= 10; messageNumber++){
+    //loop body
+    console.log(`message number ${messageNumber}`)//block of code that will be executed as long as
+    //the loop condition is true
+}
+
+// initialization step of the for loop:
+//It declares the loop variable "messageNumber" and initializes it with a value of 1.
+
+//"messageNumber <= 10"  is a condition that is checked before each iteration of the loop. 
+//If the condition is true, the loop continues to execute; if it's false, the loop terminates.
+
+// After each iteration of the loop body, an iteration step is executed:
+//"messageNumkber++" 
+//This step typically involves updating the loop variable, 
+//such as incrementing or decrementing its value. 
