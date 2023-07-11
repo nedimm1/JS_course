f5eb9b4418e3ec6d18ca1e0aac590c54a1c3a606
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-11 15:35:15 +0200

    learining about nested loops

--- part 2/the foor loop/script2.js
+++ part 2/the foor loop/script2.js
@@ -81,9 +81,19 @@ for(let i = 0; i < me.length; i++){
 }
 
 
-//looping an array backwards:
+//looping  backwards:
 
 console.log('---Backwards loop---')
 for(let i = me.length - 1; i >= 0; i--){
     console.log(me[i])
+}
+
+//loop in a loop:
+console.log('---exercise---')
+for(let exercise = 1; exercise < 4; exercise++){
+    console.log(`exercise number ${exercise}`)
+
+    for(let rep = 1; rep < 6; rep++){
+        console.log(`exercise ${exercise}: rep ${rep}`)
+    }
 }
