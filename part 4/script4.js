ad02615cce85181d6c0199bf9553ab53b183b997
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-28 16:35:26 +0200

    doing some more DOM manipulation

--- part 4/script4.js
+++ part 4/script4.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent)
+
+document.querySelector('.number').textContent = 15;
+
+document.querySelector('.guess').value = 7; 
+
+console.log(document.querySelector('.number').textContent)
+
+console.log(document.querySelector('.guess').value)
