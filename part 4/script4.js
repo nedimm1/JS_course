221c5892eedbce62020f66583d2a0108fc92f168
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-29 16:11:18 +0200

    a message in case of no number

--- part 4/script4.js
+++ part 4/script4.js
@@ -3,4 +3,8 @@
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
+
+    if(!guess){
+        document.querySelector('.message').textContent = "NaN"
+    }
 })
