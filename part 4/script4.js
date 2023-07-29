8cf82a1b32d45c14c751099cd742324428d73cae
nedimm1 <99905901+nedimm1@users.noreply.github.com>
2023-07-29 15:55:33 +0200

    working with click avents

--- part 4/script4.js
+++ part 4/script4.js
@@ -1,11 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent)
-
-document.querySelector('.number').textContent = 15;
-
-document.querySelector('.guess').value = 7; 
-
-console.log(document.querySelector('.number').textContent)
-
-console.log(document.querySelector('.guess').value)
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess)
+})
