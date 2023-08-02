729a52939bd407e30d9f13eac17b593c28b2cc24
github_username <nedimmuminovic61@gmail.com>
2023-08-02 14:02:04 +0200

    decreasing the score

--- part 4/script4.js
+++ part 4/script4.js
@@ -4,6 +4,8 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1
 
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
@@ -14,8 +16,12 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.message').textContent = 'corect number'
     } else if (guess > secretNumber) {
         document.querySelector('.message').textContent = 'too high'
+        score = score - 1;
+        document.querySelector('.score').textContent = score
     } else if (guess < secretNumber) {
         document.querySelector('.message').textContent = 'too low'
+        score = score - 1;
+        document.querySelector('.score').textContent = score
     }
 }
 
