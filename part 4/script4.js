1b908d0a62af786b0a214a02f71b5bc3c26e6271
github_username <nedimmuminovic61@gmail.com>
2023-08-02 13:43:39 +0200

    adding more features

--- part 4/script4.js
+++ part 4/script4.js
@@ -4,15 +4,21 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1
 
 document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector('.check').addEventListener('click', function() {
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
 
-    if(!guess){
+    if (!guess) {
         document.querySelector('.message').textContent = "no number"
-    }else if(guess === secretNumber){
+    } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'corect number'
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'too high'
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'too low'
     }
-})
+}
+
+)
 
 console.log(secretNumber)
