42e368ffcb118fae033091568498e4e0fba01141
github_username <nedimmuminovic61@gmail.com>
2023-08-02 13:29:10 +0200

    making the secret number

--- part 4/script4.js
+++ part 4/script4.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
 
     if(!guess){
-        document.querySelector('.message').textContent = "NaN"
+        document.querySelector('.message').textContent = "no number"
     }
 })
+
+console.log(secretNumber)
