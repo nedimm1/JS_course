d249a5b237590e1aaa9ab3e43dcf126cfa80b932
github_username <nedimmuminovic61@gmail.com>
2023-08-08 14:40:17 +0200

    using querySelectorAll

--- part 4/project2/script4.js
+++ part 4/project2/script4.js
@@ -5,3 +5,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay')
 
 const btnCloseModal = document.querySelector('.close-modal')
+//using querySelectorAll helps us in case there are more classes with the same name 
+btnsShowModal = document.querySelectorAll('.show-modal');
+//using the normal queryselector would just console log the first element with that class name
+console.log(btnsShowModal)
