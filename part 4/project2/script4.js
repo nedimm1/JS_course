9ed2282b1effdaefdb16ec088156fa255def2085
github_username <nedimmuminovic61@gmail.com>
2023-08-08 15:22:13 +0200

    addin buttons for opening and closing modals

--- part 4/project2/script4.js
+++ part 4/project2/script4.js
@@ -9,20 +9,21 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnsShowModal = document.querySelectorAll('.show-modal');
 //using the normal queryselector would just console log the first element with that class
 
-for(let i = 0; i < btnsShowModal.length; i++){
-    btnsShowModal[i].addEventListener('click', function(){
-        console.log('button clicked')
-        modal.classList.remove('hidden')
-        overlay.classList.remove('hidden')
-    })
+const openModal = function () {
+    console.log('button clicked')
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
 }
 
-btnCloseModal.addEventListener('click', function(){
+const closeModal = function () {
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
-})
+}
 
-overlay.addEventListener('click', function(){
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-})
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal)
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
