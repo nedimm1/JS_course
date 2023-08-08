927725548770aca489085a8466151df96266a569
github_username <nedimmuminovic61@gmail.com>
2023-08-08 14:50:09 +0200

    experimenting

--- part 4/project2/script4.js
+++ part 4/project2/script4.js
@@ -6,6 +6,9 @@ const overlay = document.querySelector('.overlay')
 
 const btnCloseModal = document.querySelector('.close-modal')
 //using querySelectorAll helps us in case there are more classes with the same name 
-btnsShowModal = document.querySelectorAll('.show-modal');
-//using the normal queryselector would just console log the first element with that class name
-console.log(btnsShowModal)
+const btnsShowModal = document.querySelectorAll('.show-modal');
+//using the normal queryselector would just console log the first element with that class
+
+for(let i = 0; i < btnsShowModal.length; i++){
+    console.log(btnsShowModal[i].textContent)
+}
