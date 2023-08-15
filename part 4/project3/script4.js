47cca834788169ce087ecbf605f88d0dbb764b27
github_username <nedimmuminovic61@gmail.com>
2023-08-15 13:38:54 +0200

    variables for the btns

--- part 4/project3/script4.js
+++ part 4/project3/script4.js
@@ -5,3 +5,9 @@ const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector("score--1");
 
 const diceEL = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+
+const btnRoll = document.querySelector('.btn--roll');
+
+const btnHold = document.querySelector('.btn--hold');
