04cf97ed46e9f773313d1949030a09c94b7d26ae
github_username <nedimmuminovic61@gmail.com>
2023-08-18 16:14:28 +0200

    updating the scores and making the player to win

--- part 4/project3/script4.js
+++ part 4/project3/script4.js
@@ -27,6 +27,14 @@ let activePlayer = 0;
 
 let currentScore = 0
 
+function switchPlayer(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+}
+
 btnRoll.addEventListener('click', function () {
     let randomNum = Math.trunc(Math.random() * 6) + 1
     console.log(randomNum)
@@ -38,11 +46,23 @@ btnRoll.addEventListener('click', function () {
         currentScore += randomNum
         document.getElementById(`current--${activePlayer}`).textContent = currentScore
         curentScore0EL.textContent = curentScore
+    } else {
+       switchPlayer()
+    }
+})
+
+btnHold.addEventListener('click', function () {
+
+
+    scores[activePlayer] += currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+
+    if (scores[activePlayer] >= 20) {
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        document.querySelector(`.player--${activePlayer}`).classList.remove('.player--active')
     }else{
-        document.getElementById(`current--${activePlayer}`).textContent = 0
-        currentScore = 0
-        activePlayer = activePlayer === 0 ? 1 : 0
-        player0El.classList.toggle('player--active')
-        player1El.classList.toggle('player--active')
+       switchPlayer()
     }
 })
