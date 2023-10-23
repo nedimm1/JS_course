f22e7158342fb2d4ba0ec816d2352535b8c64365
github_username <nedimmuminovic61@gmail.com>
2023-10-23 16:52:56 +0200

    destructing an array

--- part5/script5.js
+++ part5/script5.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+'use strict'
+
+const resturant = {
+   name: 'N.I.F',
+   location: "Washington street, Van Wert, Ohio, USA",
+   categories: ["from Ohio", "Halal", "made up"],
+   starterMenue: ["American breakfest", "English breakfest", "French breakfest", "Turkish breakfest"],
+
+}
+
+const arr = [1, 2, 3]
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr)
