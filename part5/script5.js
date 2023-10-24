7551bda8e5c6e60c06f56ca6051a776ab4b7cb52
github_username <nedimmuminovic61@gmail.com>
2023-10-24 16:49:45 +0200

    switching variables the easier way

--- part5/script5.js
+++ part5/script5.js
@@ -1,27 +1,27 @@
-'use strict'
+'use strict';
 
 const resturant = {
-   name: 'N.I.F',
-   location: "Washington street, Van Wert, Ohio, USA",
-   categories: ["from Ohio", "Halal", "made up"],
-   starterMenue: ["American breakfest", "English breakfest", "French breakfest", "Turkish breakfest"],
-
+    name: 'N.I.F',
+    location: "Washington street, Van Wert, Ohio, USA",
+    categories: ["from Ohio", "Halal", "made up", "adfjas'ljfasio;j", "a;kdjfha;weouhngva"],
+    starterMenue: ["American breakfast", "English breakfast", "French breakfast", "Turkish breakfast"],
+    arryay: ['one', 'two', 'three', 'four', 'five']
 }
 
-const arr = [1, 2, 3]
-const a = arr[0]
-const b = arr[1]
-const c = arr[2]
+const arr = [1, 2, 3];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
 const [x, y, z] = arr;
 console.log(x, y, z);
-console.log(arr)
+console.log(arr);
 
-let [first, second] = resturant.categories
-console.log(first, second)
 
-//switching elements without destructing
-const temp = first;
-first = second;
-second = temp;
-console.log(first, second)
+let main, secondery;
+
+[main, secondery] = resturant.arryay;
+
+[main, secondery] = [secondery, main];
+
+console.log(main, secondery);
