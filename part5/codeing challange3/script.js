3630c68574cac2db44af314ae2f670a5a72ea772
github_username <nedimmuminovic61@gmail.com>
2023-12-16 16:44:44 +0100

    finishing the coding challange

---
+++ part5/codeing challange3/script.js
@@ -0,0 +1,44 @@
+/*1. Create an array 'events' of the different game events that happened (no 
+    duplicates)
+    2. After the game has finished, is was found that the yellow card from minute 64 
+    was unfair. So remove this event from the game events log.
+    3. Compute and log the following string to the console: "An event happened, on 
+    average, every 9 minutes" (keep in mind that a game has 90 minutes)
+    4. Loop over 'gameEvents' and log each element to the console, marking 
+    whether it's in the first half or second half (after 45 min) of the game, like this:
+    [FIRST HALF] 17: ⚽ GOAL
+ */
+
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '� Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '� Substitution'],
+    [64, '� Yellow card'],
+    [69, '� Red card'],
+    [70, '� Substitution'],
+    [72, '� Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '� Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())]
+console.log(events)
+
+gameEvents.delete(64)
+console.log(gameEvents)
+
+const time = [...gameEvents.keys()].pop()
+console.log(time)
+console.log(`An event happened, on 
+average, every ${time / gameEvents.size} minutes`)
+
+for (let [time, event] of gameEvents) {
+    if (time < 45) {
+        console.log(`[first half] ${time}: ${event}`)
+    }else{
+        console.log(`[second half] ${time}: ${event}`)
+    }
+}
