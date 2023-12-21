19f8cd6cf2ff88a929377a91a543f09e6a654055
github_username <nedimmuminovic61@gmail.com>
2023-12-21 12:50:12 +0100

    replace method

--- part5/script5.js
+++ part5/script5.js
@@ -341,3 +341,17 @@ console.log(nameNormal)
 const [firstName, lastName] = nameArray
 console.log(firstName)
 console.log(lastName)
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+
+capitalizeName('onika tanya maraj petty')
