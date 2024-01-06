e9df093329aa34638cdfc32fa54d226c79140b3d
github_username <nedimmuminovic61@gmail.com>
2024-01-06 12:47:19 +0100

    finnishing the coding challenge

---
+++ part5/codeing challange 4/script.js
@@ -0,0 +1,67 @@
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+function camelCase(...words){
+  for(let i = 0; i < words.length; i++){
+    let arr = words[i].toLowerCase().split('_')
+     arr[1] = arr[1][0].toUpperCase() + arr[1].slice(1).toLowerCase()
+    words[i] = arr.join("")
+ }
+ return words
+}
+
+function organize(list){
+  let result = '';
+  for(let i = 0; i < list.length; i++){
+    result += list[i].padEnd(20) + " " + '✅'.repeat(i + 1) + '\n';
+  }
+  return result
+}
+
+console.log(organize(camelCase('underscore_case',
+'first_name',
+'Some_Variable', 
+'calculate_AGE',
+'delayed_departure')))
+
+
+ document.querySelector('button').addEventListener('click', function () {
+  let text = document.querySelector('textarea').value
+  let camelCasedAndOrganized = organize(camelCase(...text.split('\n')));
+  document.querySelector('textarea').value = camelCasedAndOrganized;
+  
+}
+
+);
