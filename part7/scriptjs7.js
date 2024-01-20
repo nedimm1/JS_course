218cbfc4c3fc2a6f0ee1fe3370fcffa6d3bcd167
github_username <nedimmuminovic61@gmail.com>
2024-01-20 09:15:41 +0100

    slice with arrays

---
+++ part7/scriptjs7.js
@@ -0,0 +1,11 @@
+//Array methodes
+
+//1.Slice
+//like with the string
+
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr.slice(2))
+console.log(arr.slice(1, 3))
+console.log(arr.slice(-1))
+console.log(arr.slice(1, -1))
