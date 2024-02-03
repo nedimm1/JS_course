6152947dde2be1c4486e7486faea1e73595f814f
github_username <nedimmuminovic61@gmail.com>
2024-02-03 16:38:42 +0100

    Timers: setTimeout

---
+++ part8/script8.js
@@ -0,0 +1,10 @@
+//timers:
+
+//setTimeout
+//execute after some time
+
+setTimeout(() => console.log('your pizza is deliverd'), 5000)
+
+//time is written is miliseconds
+
+console.log('waiting...')
