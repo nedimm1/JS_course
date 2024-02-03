2a27bd5e581c78bb1968163dfd731bc3c657de15
github_username <nedimmuminovic61@gmail.com>
2024-02-03 16:50:54 +0100

    clearTimeout

--- part8/script8.js
+++ part8/script8.js
@@ -3,8 +3,15 @@
 //setTimeout
 //execute after some time
 
-setTimeout((ing1, ing2) => console.log(`your pizza with ${ing1} and ${ing2} is deliverd`), 5000, 'spinich', 'olives')
+let ingrediants = ['spinach', 'olives']
+
+let pizzaDelivery = setTimeout((ing1, ing2) => console.log(`your pizza with ${ing1} and ${ing2} is deliverd`), 5000, ...ingrediants)
 
 //time is written is miliseconds
 
 console.log('waiting...')
+
+if (ingrediants.includes("spinach")) {
+    clearTimeout(pizzaDelivery);
+    console.log('sorry we dont have spinach.');
+}
