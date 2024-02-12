a0a2545f356a570b03d3fde974217a6b69c69b2f
github_username <nedimmuminovic61@gmail.com>
2024-02-12 13:32:16 +0100

    working with prototypes

--- part10/script10.js
+++ part10/script10.js
@@ -3,15 +3,23 @@
 //constructors
 //a way of making Objects 
 
-function Person(firstName, lastName){
+function Person(firstName, birthYear){
   this.firstName = firstName
-  this.lastName = lastName
+  this.birthYear = birthYear
 }
 
-const me = new Person('Nedim', "Muminovic")
+const me = new Person('Nedim', 2008)
 
 console.log(me)
 
 const jeff = "jeff"
 
 console.log(jeff instanceof Person, me instanceof Person)
+
+//prototype
+
+Person.prototype.calcAge = function(){
+  console.log(2024 - this.birthYear)
+}
+
+me.calcAge()
