28fac0dedde620803af90f6989e3eee839c0fb7c
github_username <nedimmuminovic61@gmail.com>
2024-02-13 14:16:16 +0100

    finnishing the coding challange

---
+++ part10/coding challange1/script10.js
@@ -0,0 +1,38 @@
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the
+ car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+function Car(make, startingSpeed){
+    this.startingSpeed = startingSpeed
+    this.make = make;
+}
+
+const BMW = new Car("BMW", 120)
+
+const Mercedes = new Car("Mercedes" ,95)
+
+Car.prototype.accelerate = function(){
+    console.log(`${this.make} is going ${this.startingSpeed + 10}km/h`)
+}
+
+Car.prototype.brake = function(){
+    console.log(`${this.make} is going ${this.startingSpeed - 5}km/h`)
+}
+
+BMW.accelerate()
+Mercedes.accelerate()
+
+BMW.brake()
+Mercedes.brake()
