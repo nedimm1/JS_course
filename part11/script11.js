37479984944e0e8a0a4233e49eac6c04a750e1ee
github_username <nedimmuminovic61@gmail.com>
2024-02-29 13:38:59 +0100

    working with popups\

--- part11/script11.js
+++ part11/script11.js
@@ -25,6 +25,21 @@ if (navigator.geolocation) {
            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
        }).addTo(map);
        
+      map.on('click', function(mapEvent){
+        const {lat, lng} = mapEvent.latlng
+
+        L.marker({lat, lng}).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup"
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+      })
+
        L.marker(coords).addTo(map)
            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
            .openPopup();
